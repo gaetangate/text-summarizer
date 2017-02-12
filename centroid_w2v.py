93946a5b9f56f5f602d0a14776a8b144aba35006
Gaetano Rossiello <gaetano81@gmail.com>
2017-02-12 11:41:17 +0100

    add substract_centroid and keep_first features to Word2Vec summarizer

--- centroid_w2v.py
+++ centroid_w2v.py
@@ -1,6 +1,10 @@
 """
-     Author: Gaetano Rossiello
-     Email: gaetano.rossiello@uniba.it
+Implementation based on:
+
+Centroid-based Text Summarization through Compositionality of Word Embeddings
+
+Author: Gaetano Rossiello
+Email: gaetano.rossiello@uniba.it
 """
 import base
 import numpy as np
@@ -16,7 +20,9 @@ class CentroidW2VSummarizer(base.BaseSummarizer):
                  debug=False,
                  topic_threshold=0.3,
                  sim_threshold=0.95,
-                 reordering=True):
+                 reordering=True,
+                 subtract_centroid=False,
+                 keep_first=False):
         super().__init__(language, stopwords_remove, debug)
 
         self.word2vec = Word2Vec.load_word2vec_format(word2vec_model_path, binary=True)
@@ -26,6 +32,18 @@ class CentroidW2VSummarizer(base.BaseSummarizer):
         self.topic_threshold = topic_threshold
         self.sim_threshold = sim_threshold
         self.reordering = reordering
+
+        self.keep_first = keep_first
+        self.subtract_centroid = subtract_centroid
+
+        # Create centroid of all vector space
+        count = 0
+        self.centroid_space = np.zeros(self.word2vec.vector_size, dtype="float32")
+        for w in self.index2word_set:
+            self.centroid_space = self.centroid_space + self.word2vec[w]
+            count += 1
+        if count != 0:
+            self.centroid_space = np.divide(self.centroid_space, count)
         return
 
     def get_topic_idf(self, sentences):
@@ -44,35 +62,56 @@ class CentroidW2VSummarizer(base.BaseSummarizer):
         word_list = []
         for i in range(centroid_vector.shape[0]):
             if centroid_vector[i] > self.topic_threshold:
-                print(feature_names[i], centroid_vector[i])
+                # print(feature_names[i], centroid_vector[i])
                 word_list.append(feature_names[i])
 
         return word_list
 
     def word_vectors_cache(self, sentences):
+        self.word_vectors = dict()
         for s in sentences:
             words = s.split()
             for w in words:
-                if w not in self.word_vectors and w in self.index2word_set:
-                    self.word_vectors[w] = self.word2vec[w]
+                if w in self.index2word_set:
+                    if self.subtract_centroid:
+                        self.word_vectors[w] = (self.word2vec[w] - self.centroid_space)
+                    else:
+                        self.word_vectors[w] = self.word2vec[w]
         return
 
     # Sentence representation with sum of word vectors
-    def compose_vectors(self, vectors):
+    def compose_vectors(self, words):
         composed_vector = np.zeros(self.word2vec.vector_size, dtype="float32")
-        count = 0
         word_vectors_keys = set(self.word_vectors.keys())
-        for vector in vectors:
-            if vector in word_vectors_keys:
-                composed_vector = composed_vector + self.word_vectors[vector]
+        count = 0
+        for w in words:
+            if w in word_vectors_keys:
+                composed_vector = composed_vector + self.word_vectors[w]
                 count += 1
-
+        if count != 0:
+            composed_vector = np.divide(composed_vector, count)
         return composed_vector
 
     def summarize(self, text, limit_type='word', limit=100):
         raw_sentences = self.sent_tokenize(text)
         clean_sentences = self.preprocess_text(text)
+
+        if self.debug:
+            print("ORIGINAL TEXT STATS = {0} chars, {1} words, {2} sentences".format(len(text),
+                                                                                     len(text.split(' ')),
+                                                                                     len(raw_sentences)))
+            print("*** RAW SENTENCES ***")
+            for i, s in enumerate(raw_sentences):
+                print(i, s)
+            print("*** CLEAN SENTENCES ***")
+            for i, s in enumerate(clean_sentences):
+                print(i, s)
+
         centroid_words = self.get_topic_idf(clean_sentences)
+
+        if self.debug:
+            print("*** CENTROID WORDS ***")
+            print(len(centroid_words), centroid_words)
 
         self.word_vectors_cache(clean_sentences)
         centroid_vector = self.compose_vectors(centroid_words)
@@ -85,10 +124,23 @@ class CentroidW2VSummarizer(base.BaseSummarizer):
             sentences_scores.append((i, raw_sentences[i], score, sentence_vector))
 
         sentence_scores_sort = sorted(sentences_scores, key=lambda el: el[2], reverse=True)
-        # print(sentence_scores_sort)
+        if self.debug:
+            print("*** SENTENCE SCORES ***")
+            for s in sentence_scores_sort:
+                print(s[0], s[1], s[2])
 
         count = 0
         sentences_summary = []
+
+        if self.keep_first:
+            for s in sentence_scores_sort:
+                if s[0] == 0:
+                    sentences_summary.append(s)
+                    if limit_type == 'word':
+                        count += len(s[1].split())
+                    else:
+                        count += len(s[1])
+
         for s in sentence_scores_sort:
             if count > limit:
                 break
@@ -110,5 +162,13 @@ class CentroidW2VSummarizer(base.BaseSummarizer):
             sentences_summary = sorted(sentences_summary, key=lambda el: el[0], reverse=False)
 
         summary = "\n".join([s[1] for s in sentences_summary])
+
+        if self.debug:
+            print("SUMMARY TEXT STATS = {0} chars, {1} words, {2} sentences".format(len(summary),
+                                                                                    len(summary.split(' ')),
+                                                                                    len(sentences_summary)))
+
+            print("*** SUMMARY ***")
+            print(summary)
 
         return summary
