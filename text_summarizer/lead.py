162f5abb1217f965788f783feef37731f2678fbf
Olivier <olivier@compilatio.net>
2019-04-04 13:22:52 +0200

    modified lead.py so the stopword remover actually remove words!

--- text_summarizer/lead.py
+++ text_summarizer/lead.py
@@ -6,17 +6,20 @@ from text_summarizer import base
 
 
 class LeadSummarizer(base.BaseSummarizer):
-
-    def __init__(self,
-                 language='english',
-                 preprocess_type='nltk',
-                 length_limit=10,
-                 stopwords_remove=True,
-                 debug=False):
-        super().__init__(language, preprocess_type, stopwords_remove, length_limit, debug)
+    def __init__(
+        self,
+        language="english",
+        preprocess_type="nltk",
+        length_limit=10,
+        stopwords_remove=True,
+        debug=False,
+    ):
+        super().__init__(
+            language, preprocess_type, stopwords_remove, length_limit, debug
+        )
         return
 
-    def summarize(self, text, limit_type='word', limit=100):
+    def summarize(self, text, limit_type="word", limit=100):
         raw_sentences = self.sent_tokenize(text)
         count = 0
         sentences_summary = []
@@ -24,10 +27,11 @@ class LeadSummarizer(base.BaseSummarizer):
             if count > limit:
                 break
             sentences_summary.append(s)
-            if limit_type == 'word':
+            print("adding ", s)
+            if limit_type == "word":
                 count += len(s.split())
             else:
                 count += len(s)
 
-        summary = "\n".join([s for s in sentences_summary])
+        summary = "\n*\n".join([s for s in sentences_summary])
         return summary
